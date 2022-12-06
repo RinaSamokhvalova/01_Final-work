29a73d1cb11e0bcdd95b094bca6aaa113b518740
«rina.sanokhvalova» <rina.samokhvalova@mail.ru>
2022-12-06 18:52:13 +0300

    solution v02

--- task/Program.cs
+++ task/Program.cs
@@ -2,36 +2,48 @@
 // формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
 // Примеры:
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
+// ["Hello", "2", "world", ":-)"] → ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] → ["-2"]
+// ["Russia", "Denmark", "Kazan"] → []
 
-string[] arrayA = { "Hello", "2", "world", ":-)" };   // - ввод исходного одномерного массива, массив взят из примера
-int numberСharacters = 3;                             // - переменная - количество символов, критерий отбора элементов
-string[] arrayB = new string[arrayA.Length];          // - инициализация нового массива длинной равной длинне исходного массива
-int count = 0;                                        // - инициализация вспомогательного счетчика
+// Вариант решения - 2
+// Решение оформлено в метод. Ввод данных - изначально заданные параметры.
 
-// Создание нового массива, путем отбора элементов нужной длинны 
-for (int i = 0; i < arrayA.Length; i++)
+// используем для старта набор данных из примера:
+string[] arrayA = { "Hello", "2", "world", ":-)" };                     // - ввод исходного одномерного массива (массив взят из примера)
+// string[] arrayA = { "1234", "1567", "-2", "computer science" };
+// string[] arrayA = { "Russia", "Denmark", "Kazan" };
+int numberСharacters = 3;                                               // - количество символов, критерий отбора элементов
+
+Console.WriteLine("Исходный массив:");
+Console.WriteLine(String.Join(" ", arrayA));                            // - вывод исходного одномерного массива
+
+string[] arrayB = GetSelectionArray(arrayA, numberСharacters);          // - создание нового массива            
+Console.WriteLine("Новый массив:");
+Console.WriteLine(String.Join(" ", arrayB));
+
+// Метод осздает новый массив на основе заданного, отсортировывая данные по заданной длинне строки
+string[] GetSelectionArray(string[] array, int number)
 {
-    if (arrayA[i].Length <= numberСharacters)
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)          // считаем количество элементов в новом массиве
     {
-        arrayB[count] = arrayA[i];
-        count++;
+        if (array[i].Length <= number)
+        {
+            count++;
+        }
     }
-}
 
-// Вывод результатов решения
-Console.WriteLine("Исходный массив");
-for (int i = 0; i < arrayA.Length; i++)
-{
-    Console.WriteLine($"arrayA[{i}] = {arrayA[i]} ");
+    string[] res = new string[count];               // инициализация нового массива
+    count = 0;
+    for (int i = 0; i < array.Length; i++)          // заполнение нового массива
+    {
+        if (array[i].Length <= number)
+        {
+            res[count] = arrayA[i];
+            count++;
+        }
+    }
+    return res;
 }
-Console.WriteLine($"Новый массив из элементов длинна которых меньше или равна {numberСharacters}");
-for (int i = 0; i < count; i++)
-{
-    Console.WriteLine($"arrayB[{i}] = {arrayB[i]} ");
-}
-Console.WriteLine();
